8d2daa340590a7016dd8d12238134785d676e7fe
condron <chris@condron.net>
2018-07-20 16:24:16 -0400

    fix ci build by adding a placeholder test in UI.Testing

---
+++ src/ReactiveDomain.UI.Tests/place_holder_test.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Xunit;
+
+namespace ReactiveDomain.UI.Tests
+{
+    public class place_holder_test
+    {
+        [Fact(Skip = "placeholder")]
+        public void not_a_test() {
+            throw new NotImplementedException();
+        }
+    }
+}
