c99ef0098e0e8fe8a72538cec9003581aa138355
condron <chris@condron.net>
2018-07-20 16:12:51 -0400

    refactor UI project out of Foundation and add enhanced ReadModel support

---
+++ src/ReactiveDomain.Foundation/StreamStore/ReadModelState.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ReactiveDomain.Util;
+
+namespace ReactiveDomain.Foundation.EventStore
+{
+    public class ReadModelState
+    {
+        public readonly string ModelName;
+        public readonly List<Tuple<string, long>> Checkpoints;
+        public readonly object State;
+
+        public ReadModelState(
+            string modelName,
+            List<Tuple<string,long>> checkpoint,
+            object state) {
+            Ensure.NotNullOrEmpty(modelName,nameof(modelName));
+            ModelName = modelName;
+            Checkpoints = checkpoint;
+            State = state;
+        }
+    }
+}
