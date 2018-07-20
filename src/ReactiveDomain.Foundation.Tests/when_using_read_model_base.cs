9c1060e4ae07a1db56f5d75b822fa343b9b61286
condron <chris@condron.net>
2018-07-20 18:17:03 -0400

    fix typo in travis and complete model base test class

--- src/ReactiveDomain.Foundation.Tests/when_using_read_model_base.cs
+++ src/ReactiveDomain.Foundation.Tests/when_using_read_model_base.cs
@@ -4,10 +4,30 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using ReactiveDomain.Testing;
+using ReactiveDomain.Testing.EventStore;
 using Xunit;
 
 namespace ReactiveDomain.Foundation.Tests.SynchronizedStreamListenerTests
 {
-    public class when_using_read_model_base :         ReadModelBase,        IClassFixture<StreamStoreConnectionFixture>{        
+    public class when_using_read_model_base : 
+        ReadModelBase
+    {
+        
+        private static IListener GetListener() {
+            return new SynchronizableStreamListener(
+                        nameof(when_using_read_model_base),
+                        conn,
+                        namer,
+                        serializer,
+                        true);
+        }
+        private static IStreamStoreConnection conn = new MockStreamStoreConnection(nameof(when_using_read_model_base));
+        private static IEventSerializer serializer = new JsonMessageSerializer();
+        private static IStreamNameBuilder namer = new PrefixedCamelCaseStreamNameBuilder(nameof(when_using_read_model_base));
+
+        public when_using_read_model_base()
+                    :base(nameof(when_using_read_model_base),GetListener) {
+            
+        }        
     }
 }
