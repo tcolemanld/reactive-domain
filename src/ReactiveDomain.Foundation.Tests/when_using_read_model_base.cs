1f9d6776653a15337f5b6f8f1d33a817540b75fe
condron <chris@condron.net>
2018-07-20 17:41:27 -0400

    clean build and update UI.Test project

---
+++ src/ReactiveDomain.Foundation.Tests/when_using_read_model_base.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ReactiveDomain.Testing;
+using Xunit;
+
+namespace ReactiveDomain.Foundation.Tests.SynchronizedStreamListenerTests
+{
+    public class when_using_read_model_base :         ReadModelBase,        IClassFixture<StreamStoreConnectionFixture>{        
+    }
+}
