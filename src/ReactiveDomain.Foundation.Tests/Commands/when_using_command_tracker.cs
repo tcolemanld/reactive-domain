6830f5e40e178d599612f77582a4c39a590cbb6d
condron <chris@condron.net>
2018-08-21 14:43:09 -0400

    add tracker test

--- src/ReactiveDomain.Foundation.Tests/Commands/when_using_command_tracker.cs
+++ src/ReactiveDomain.Foundation.Tests/Commands/when_using_command_tracker.cs
@@ -10,6 +10,7 @@ using static System.Threading.Interlocked;
 using CanceledAckTimeout = ReactiveDomain.Foundation.Commands.CanceledAckTimeout;
 
 namespace ReactiveDomain.Foundation.Tests.Commands {
+
     // ReSharper disable once InconsistentNaming
     public class when_using_command_tracker {
         private readonly TestTimeSource _timeSource = new TestTimeSource();
@@ -34,6 +35,7 @@ namespace ReactiveDomain.Foundation.Tests.Commands {
             CommandResponse response = null;
             Task.Run(() => response = t.Send());
             AssertEx.IsOrBecomesTrue(() => Read(ref gotCmd) == 1);
+            t.Handle(new CommandTracker.CommandReceived(cmd, Guid.Empty));
             t.Handle(cmd.Succeed());
             AssertEx.IsOrBecomesTrue(() => Read(ref gotTComplete) == 1);
             AssertEx.IsOrBecomesTrue(() => response is Success);
@@ -93,5 +95,77 @@ namespace ReactiveDomain.Foundation.Tests.Commands {
             AssertEx.IsOrBecomesTrue(() => Read(ref gotCancel) == 1);
             AssertEx.IsOrBecomesTrue(() => (response as CanceledCompletionTimeout)?.Exception is TimeoutException);
         }
+        [Fact]
+        public void command_tracker_can_be_disposed() {
+            var gotCmd = 0L;
+            var gotTComplete = 0L;
+            var gotCancel = 0L;
+            var bus = new InMemoryBus("temp");
+            bus.Subscribe(new AdHocHandler<TestCommands.Command1>(_ => Increment(ref gotCmd)));
+            bus.Subscribe(new AdHocHandler<CommandTracker.CommandProcessingCompleted>(_ => Increment(ref gotTComplete)));
+            bus.Subscribe(new AdHocHandler<CommandTracker.CommandCancellationRequested>(_ => Increment(ref gotCancel)));
+            var cmd = new TestCommands.Command1();
+            var t = new CommandTracker(
+                cmd,
+                bus,
+                bus,
+                _ackTimeout,
+                _completionTimeout,
+                _timeSource);
+            CommandResponse response = null;
+            Task.Run(() => response = t.Send());
+            AssertEx.IsOrBecomesTrue(() => Read(ref gotCmd) == 1);
+            t.Dispose();
+
+            AssertEx.IsOrBecomesTrue(() => Read(ref gotTComplete) == 0);
+            AssertEx.IsOrBecomesTrue(() => Read(ref gotCancel) == 1);
+            AssertEx.IsOrBecomesTrue(() => response is Canceled);
+        }
+        [Fact]
+        public void publish_failure_will_fail_command() {
+            var gotTComplete = 0L;
+            var bus = new InMemoryBus("temp");
+            bus.Subscribe(new AdHocHandler<TestCommands.Command1>(_ => throw new TestException()));
+            bus.Subscribe(new AdHocHandler<CommandTracker.CommandProcessingCompleted>(_ => Increment(ref gotTComplete)));
+            var cmd = new TestCommands.Command1();
+            var t = new CommandTracker(
+                cmd,
+                bus,
+                bus,
+                _ackTimeout,
+                _completionTimeout,
+                _timeSource);
+            CommandResponse response = null;
+            Task.Run(() => response = t.Send());
+         
+
+            AssertEx.IsOrBecomesTrue(() => Read(ref gotTComplete) == 1);
+            AssertEx.IsOrBecomesTrue(() => (response as Fail)?.Exception is TestException);
+        }
+        [Fact]
+        public void cannot_send_twice() {
+            var gotTComplete = 0L;
+            var done = 0L;
+            var bus = new InMemoryBus("temp");
+            bus.Subscribe(new AdHocHandler<TestCommands.Command1>(_ => SpinWait.SpinUntil(()=>Read(ref done) == 1)));
+            bus.Subscribe(new AdHocHandler<CommandTracker.CommandProcessingCompleted>(_ => Increment(ref gotTComplete)));
+            var cmd = new TestCommands.Command1();
+            var t = new CommandTracker(
+                cmd,
+                bus,
+                bus,
+                _ackTimeout,
+                _completionTimeout,
+                _timeSource);
+            CommandResponse response = null;
+            Task.Run(() => response = t.Send());
+            t.Send(false);
+            done++;
+         
+
+            AssertEx.IsOrBecomesTrue(() => Read(ref gotTComplete) == 1);
+            AssertEx.IsOrBecomesTrue(() => (response as Fail)?.Exception is InvalidOperationException);
+        }
+        public class TestException:Exception{}
     }
 }
