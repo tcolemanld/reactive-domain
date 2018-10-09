tcolemanld/reactive-domain

commit f6dbf4dfaadf47fc646a63cda502fbc2a3bb0c81

fix tcp serialization message type identification

src/ReactiveDomain.Transport/TcpBusServerSide.cs
@@ -15,26 +15,21 @@ namespace ReactiveDomain.Transport
             int commandPort,
             IGeneralBus messageBus)
             : base(hostIp, commandPort, messageBus)
-        {           
-            _commandPortListener = ConfigureTcpListener(CommandEndpoint, TcpMessageArrived);
-        }       
-
-        private TcpServerListener ConfigureTcpListener(IPEndPoint hostEndPoint, Action<ArraySegment<byte>> handler)
         {
-            Log.Info("ConfigureTcpListener(" + hostEndPoint.AddressFamily + ", " + hostEndPoint.Port + ") entered.");
-          
-            LengthPrefixMessageFramer framer = new LengthPrefixMessageFramer();
-            framer.RegisterMessageArrivedCallback(handler);
-
-            var listener = new TcpServerListener(hostEndPoint);
+           
+            Log.Info("ConfigureTcpListener(" + CommandEndpoint.AddressFamily + ", " + CommandEndpoint.Port + ") entered.");
+            
+            var listener = new TcpServerListener(CommandEndpoint);
 
             listener.StartListening((endPoint, socket) =>
             {
-                TcpConnection = Transport.TcpConnection.CreateAcceptedTcpConnection(Guid.NewGuid(), endPoint, socket, verbose: true);
+               var conn = Transport.TcpConnection.CreateAcceptedTcpConnection(Guid.NewGuid(), endPoint, socket, verbose: true);
 
                 Action<ITcpConnection, IEnumerable<ArraySegment<byte>>> callback = null;
                 callback = (x, data) =>
                 {
+                    LengthPrefixMessageFramer framer = new LengthPrefixMessageFramer();
+                    framer.RegisterMessageArrivedCallback(TcpMessageArrived);
                     try
                     {
                         framer.UnFrameData(data);
@@ -45,13 +40,13 @@ namespace ReactiveDomain.Transport
                         // SendBadRequestAndClose(Guid.Empty, string.Format("Invalid TCP frame received. Error: {0}.", exc.Message));
                         return;
                     }
-                    TcpConnection.ReceiveAsync(callback);
+                    conn.ReceiveAsync(callback);
                 };
-                TcpConnection.ReceiveAsync(callback);
+                conn.ReceiveAsync(callback);
+                TcpConnection.Add(conn);
             }, "Standard");
-            Log.Info("ConfigureTcpListener(" + hostEndPoint.AddressFamily + ", " + hostEndPoint.Port + ") successfully constructed TcpServerListener.");
-            return listener;
+            Log.Info("ConfigureTcpListener(" + CommandEndpoint.AddressFamily + ", " + CommandEndpoint.Port + ") successfully constructed TcpServerListener.");
+            _commandPortListener = listener;
         }
-
     }
 }

src/ReactiveDomain.Foundation/EventStore/StreamListener.cs
@@ -3,6 +3,7 @@ using System.Threading;
 using ReactiveDomain.Messaging;
 using ReactiveDomain.Messaging.Bus;
 using ReactiveDomain.Util;
+using ReactiveDomain.EventStore;
 
 namespace ReactiveDomain.Foundation.EventStore {
     public class StreamListener : IListener {
@@ -34,7 +35,18 @@ namespace ReactiveDomain.Foundation.EventStore {
             ListenerName = listenerName;
             _streamNameBuilder = streamNameBuilder;
         }
-
+        /// <summary>
+        /// Event Stream Listener
+        /// i.e. $et-[MessageType]
+        /// </summary>
+        /// <param name="tMessage"></param>
+        /// <param name="checkpoint"></param>
+        /// <param name="blockUntilLive"></param>
+        /// <param name="millisecondsTimeout"></param>
+        public void Start(Type tMessage, int? checkpoint = null, bool blockUntilLive = false, int millisecondsTimeout = 1000) {
+            if (!tMessage.IsSubclassOf(typeof(Event))) throw new ArgumentException("type must derive from ReactiveDomain.Messaging.Event", nameof(tMessage));
+            Start(tMessage.GetEventTypeStreamName(), checkpoint, blockUntilLive, millisecondsTimeout);
+        }
         /// <summary>
         /// Category Stream Listener
         /// i.e. $ce-[AggregateType]

src/ReactiveDomain.Messaging/Message.cs
@@ -1,13 +1,13 @@
 
 // Based on InMemoryBus from EventStore LLP
-// Added support for updating registered types and handlers from dynamicly loaded assemblies
+// Added support for updating registered types and handlers from dynamically loaded assemblies
 // Registered an event handler for the AssemblyLoad event.
 // New assemblies are restricted to sub folders of the working directory to avoid loading system assemblies and throwing errors on reflection
-// Added cross lookups for type by typeId 
+// Added cross look-ups for type by typeId 
 // Added Types Updated event 
 // See also changes in InMemoryBus.cs 
 // Key test cases include when a assembly containing types derived from Message is loaded after the InMemoryBus is created and  top level handler (i.e. for type Message) was previously added
-// A simple example is a test fixture that sets up the bus and a top level listener in a contructor prior to executing a test cases based on types in a related project (see domain and domain.tests)
+// A simple example is a test fixture that sets up the bus and a top level listener in a constructor prior to executing a test cases based on types in a related project (see domain and domain.tests)
 // Chris Condron 3-4-2014
 
 
@@ -53,6 +53,7 @@ namespace ReactiveDomain.Messaging
         public static Dictionary<Type, int[]> DescendantsByType;
         private static readonly Dictionary<Type, int> MsgTypeIdByType;
         private static readonly Dictionary<int, Type> MsgTypeByTypeId;
+        private static readonly Dictionary<string, Type> MsgTypeByFullName;
 
         public static int MaxMsgTypeId;
         private static object _typeLoaderLock = new object();
@@ -60,6 +61,7 @@ namespace ReactiveDomain.Messaging
         {
             MsgTypeIdByType = new Dictionary<Type, int>();
             MsgTypeByTypeId = new Dictionary<int, Type>();
+            MsgTypeByFullName = new Dictionary<string, Type>();
             Setup();
             AppDomain.CurrentDomain.AssemblyLoad += CurrentDomain_AssemblyLoad;
         }
@@ -84,7 +86,7 @@ namespace ReactiveDomain.Messaging
             }
         }
 
-        private static readonly HashSet<string> _knownAssemblies = new HashSet<string>(StringComparer.OrdinalIgnoreCase);
+        private static readonly HashSet<string> KnownAssemblies = new HashSet<string>(StringComparer.OrdinalIgnoreCase);
         private static void Setup()
         {
             lock (_typeLoaderLock)
@@ -102,9 +104,9 @@ namespace ReactiveDomain.Messaging
                 {
                     try
                     {
-                        if (_knownAssemblies.Contains(assembly.FullName))
+                        if (KnownAssemblies.Contains(assembly.FullName))
                             continue;
-                        _knownAssemblies.Add(assembly.FullName);
+                        KnownAssemblies.Add(assembly.FullName);
                         if (assembly.FullName.StartsWith("Telerik", StringComparison.OrdinalIgnoreCase))
                             continue;
                         foreach (var msgType in assembly.GetLoadableTypes().Where(rootMsgType.IsAssignableFrom))
@@ -123,6 +125,7 @@ namespace ReactiveDomain.Messaging
                             {
                                 MsgTypeIdByType.Add(msgType, msgTypeId);
                                 MsgTypeByTypeId.Add(msgTypeId, msgType);
+                                MsgTypeByFullName.Add(msgType.FullName, msgType);
                                 typesAdded = true; //mark it so we can fire the event once per loaded assembly
                             }
                             parents.Add(msgTypeId, new List<int>());
@@ -284,11 +287,17 @@ namespace ReactiveDomain.Messaging
             if (MsgTypeByTypeId.TryGetValue(id, out type)) return type;
             throw new UnregisteredMessageException($"No message registered for TypeId {id}.");
         }
+        public static Type GetMsgType(string fullname)
+        {
+            Type type;
+            if (MsgTypeByFullName.TryGetValue(fullname, out type)) return type;
+            throw new UnregisteredMessageException($"No message registered for type name {fullname}.");
+        }
     }
 
     public class MessagingException : Exception
     {
-        public MessagingException() : base() { }
+        public MessagingException() { }
         public MessagingException(string message) : base(message) { }
         public MessagingException(string message, Exception innerException) : base(message, innerException) { }
     }

src/ReactiveDomain.Persistence/EventStore/EventStoreClientUtils.cs
@@ -63,7 +63,10 @@ namespace ReactiveDomain.EventStore {
         //{
         //    return @event.DeserializeEvent() as DomainEvent;
         //}
-
+        public static string GetEventTypeStreamName(this Type typeofAggregateDomainObject)
+        {
+            return $"{EventTypeStreamNamePrefix}-{typeofAggregateDomainObject.Name}";
+        }
 
 
 

src/ReactiveDomain.Testing/EventStore/MockStreamStoreConnectionTests.cs
@@ -18,7 +18,8 @@ namespace ReactiveDomain.Testing {
             _streamNameBuilder = new PrefixedCamelCaseStreamNameBuilder("UnitTest");
             _bus = new InMemoryBus("Mock Event Store Post Commit Target");
             //todo: reconnect bus to the all stream subscription
-            _repos.Add(new StreamStoreRepository(_streamNameBuilder, new MockStreamStoreConnection("Test")));
+            //todo: re-add MockStreamStoreConnection
+            //_repos.Add(new StreamStoreRepository(_streamNameBuilder, new MockStreamStoreConnection("Test")));
             _repos.Add(new StreamStoreRepository(_streamNameBuilder, fixture.Connection));
         }
         //TODO: Add Subscription Tests

src/ReactiveDomain.Transport.Tests/MockTcpConnection.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Net;
 using System.Net.Sockets;
 
-namespace ReactiveDomain.Transport.Tests.Transport
+namespace ReactiveDomain.Transport.Tests
 {
     public class MockTcpConnection : ITcpConnection
     {

src/ReactiveDomain.Transport.Tests/ReactiveDomain.Transport.Tests.csproj
@@ -1,5 +1,5 @@
 <Project Sdk="Microsoft.NET.Sdk">
-  <Import Project="../ci.build.imports"/>
+  <Import Project="../ci.build.imports" />
   <PropertyGroup>
     <TargetFrameworks>$(TestTargetFrameworks)</TargetFrameworks>
     <IsTestProject>true</IsTestProject>

src/ReactiveDomain.Transport.Tests/TcpBusClientSideTests.cs
@@ -2,7 +2,7 @@
 using System.Net;
 using ReactiveDomain.Messaging.Bus;
 
-namespace ReactiveDomain.Transport.Tests.Transport
+namespace ReactiveDomain.Transport.Tests
 {
     public class TcpBusClientSideTests
     {

commit d4c90d914774a3ea5554df342ccdf194b30799be

Reuse the same framer across receive callbacks for a given connection
Otherwise, if the the frame is received in pieces, the new framer will
not know, and incorrectly try to read a header in the middle of a
frame.

This bug was introduced in commit
f6dbf4dfaadf47fc646a63cda502fbc2a3bb0c81 when TcpBusSide gained multiple
connections instead of one. It looks like the idea in constructing the
new framer was to stop the framer from being shared between connections.

I have placed the framer construction so that each connection has its
own, and reuses it on each receive. I have also added a unit test
that reproduces the bug.

src/ReactiveDomain.Transport/TcpBusServerSide.cs
@@ -25,11 +25,12 @@ namespace ReactiveDomain.Transport
             {
                var conn = Transport.TcpConnection.CreateAcceptedTcpConnection(Guid.NewGuid(), endPoint, socket, verbose: true);
 
+                LengthPrefixMessageFramer framer = new LengthPrefixMessageFramer();
+                framer.RegisterMessageArrivedCallback(TcpMessageArrived);
+
                 Action<ITcpConnection, IEnumerable<ArraySegment<byte>>> callback = null;
                 callback = (x, data) =>
                 {
-                    LengthPrefixMessageFramer framer = new LengthPrefixMessageFramer();
-                    framer.RegisterMessageArrivedCallback(TcpMessageArrived);
                     try
                     {
                         framer.UnFrameData(data);

src/ReactiveDomain.Transport.Tests/TcpBusServerSideTests.cs
@@ -1,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading;
+using System.Threading.Tasks;
+using ReactiveDomain.Messaging;
+using ReactiveDomain.Messaging.Bus;
+using Xunit;
+
+namespace ReactiveDomain.Transport.Tests
+{
+    public class TcpBusServerSideTests
+    {
+        [Fact]
+        public void can_handle_split_frames()
+        {
+            // 16kb large enough to cause the transport to split up the frame.
+            // it would be better if we did the splitting manually so we were sure it really happened.
+            // would require mocking more things.
+            var hostAddress = IPAddress.Loopback;
+            var prop1 = "prop1";
+            var prop2 = string.Join("", Enumerable.Repeat("a", 16 * 1024));
+            var port = 10000;
+            var tcs = new TaskCompletionSource<Message>();
+
+            // server side
+            var serverInbound = new QueuedHandler(
+                new AdHocHandler<Message>(tcs.SetResult),
+                "InboundMessageQueuedHandler",
+                true,
+                TimeSpan.FromMilliseconds(1000));
+
+            var tcpBusServerSide = new TcpBusServerSide(hostAddress, port, null)
+            {
+                InboundMessageQueuedHandler = serverInbound,
+                InboundSpamMessageTypes = new List<Type>(),
+            };
+
+            serverInbound.Start();
+
+            // client side
+            var tcpBusClientSide = new TcpBusClientSide(null, hostAddress, port);
+
+            // wait for tcp connection to be established (maybe an api to detect this would be nice)
+            Thread.Sleep(TimeSpan.FromMilliseconds(200));
+
+            // put message into client
+            tcpBusClientSide.Handle(new WoftamEvent(prop1, prop2));
+
+            // expect to receive it in the server
+            var gotMessage = tcs.Task.Wait(TimeSpan.FromMilliseconds(1000));
+            Assert.True(gotMessage);
+            var evt = Assert.IsType<WoftamEvent>(tcs.Task.Result);
+            Assert.Equal(prop1, evt.Property1);
+            Assert.Equal(prop2, evt.Property2);
+        }
+    }
+}